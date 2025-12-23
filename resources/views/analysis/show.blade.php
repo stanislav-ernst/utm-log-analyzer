198423377b2dfd3caee805af75d6757e665121f3
Stanislav Ernst <stanislav_ernst@outlook.de>
2025-12-23 11:15:14 +0100

    Blade-Template erweitert: Fehlerhinweis nur bei fehlerhaften Zeilen angezeigt.

--- resources/views/analysis/show.blade.php
+++ resources/views/analysis/show.blade.php
@@ -29,10 +29,14 @@
                     <p class="mb-1">Quelle: {{ $run->source_file }}</p>
                     <p class="mb-1">Verarbeitete Zeilen: {{ $run->parsed_lines }}</p>
                     <p class="mb-5">Fehlerhafte Zeilen: {{ $run->error_lines }}</p>
-                    <div role="alert" class="mt-3 mb-5 relative flex w-full p-3 text-sm text-white bg-slate-800 rounded-md">
-                        <svg class="h-5 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#e8d7e8" d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zM224 160a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm-8 64l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
-                        Bei der Auswertung wurden fehlerhafte Zeilen ignoriert.
-                    </div>
+                    @if($run->error_lines > 0)
+                        <div role="alert" class="mt-3 mb-5 relative flex w-full p-3 text-sm text-white bg-slate-800 rounded-md">
+                            <svg class="h-5 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path fill="#e8d7e8" d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zM224 160a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm-8 64l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/>
+                            </svg>
+                            Bei der Auswertung wurden fehlerhafte Zeilen ignoriert.
+                        </div>
+                    @endif
                     <h2 class="text-xl mb-2">Top Lizenzen</h2>
                     <div class="relative flex flex-col w-full overflow-scroll text-slate-300 bg-slate-800 shadow-md rounded-lg bg-clip-border mb-5">
                         <table class="w-full text-left table-auto min-w-max">
